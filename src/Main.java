b99bdebba122780b5fd85816efa2fd94dc57b7ee
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-09 14:30:07 -0500

    asd

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    
+        Scanner sc = new Scanner(System.in);
+        double a;
+        System.out.println("Enter a: ");
+        a =sc.nextInt();
+        double b;
+        System.out.println("Enter b :");
+        b =sc.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping a = "+a);
+        System.out.println("After swapping a = "+b);
 }
 
 }
